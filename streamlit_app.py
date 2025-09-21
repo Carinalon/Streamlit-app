67255eacf442c327dab3c67ee0aec1d407c5dbcc
Carina <146588583+Carinalon@users.noreply.github.com>
2025-09-21 23:35:46 +0000

    titulo en español y slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My nueva app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider('Selecciona la cantidad')
+
+st.write(f'la cantidad seleccionada es: {cantidad}')
